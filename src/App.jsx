73aa174ba4ca1b4de76bef418eb1c35c0162e74b
nmartin <nate.martin27@gmail.com>
2024-12-12 12:21:53 -0500

    Macro ready to fetch

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <>
-    <h1>Hello world!</h1>
+    <h1>Welcome to the Macro App!</h1>
     <Macro />
     </>
   );
--- src/components/macro.jsx
+++ src/components/macro.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Macro = () => {
+  const [foods, setFoods] = useState([]);
+  const fetchFoods = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/macros`);
+      const foodData = await response.json();
+      console.log(foodData);
+      setFoods(foodData);
+    } catch (error) {
+      console.error("Error Fetching Food Macros:", error);
+    }
+  };
+  useEffect(() => {
+    fetchFoods();
+  }, []);
 
-
-
-
-
-
-
-    return (
-<>
-<h1>Test</h1>
-</>
-    );
-}
-
+  return (
+    <>
+      <h1>Foods</h1>
+      <div>
+        {foods.map((food, index) => (
+          <div key={index}>
+            <p>Name: {food.name}</p>
+            <p>Calories: {food.calories}</p>
+            <p>Protein: {food.protein}</p>
+            <p>Carbs: {food.carbs}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Macro;
