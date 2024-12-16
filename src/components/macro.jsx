f3b62c322702681009c97ab230e8845623c63277
nmartin <nate.martin27@gmail.com>
2024-12-16 12:52:51 -0500

    added bootstrap

--- src/components/macro.jsx
+++ src/components/macro.jsx
@@ -4,7 +4,7 @@ const Macro = () => {
   const [foods, setFoods] = useState([]);
   const fetchFoods = async () => {
     try {
-      const response = await fetch(`http://localhost:3000/macros`);
+      const response = await fetch(`http://localhost:3001/macros`);
       const foodData = await response.json();
       console.log(foodData);
       setFoods(foodData);
@@ -19,10 +19,10 @@ const Macro = () => {
   return (
     <>
       <h1>Foods</h1>
-      <div>
+      <div className="foodsCardDiv">
         {foods.map((food, index) => (
-          <div key={index}>
-            <p>Name: {food.name}</p>
+          <div className="foodsCard" key={index}>
+            <p>Name: {food.food} </p>
             <p>Calories: {food.calories}</p>
             <p>Protein: {food.protein}</p>
             <p>Carbs: {food.carbs}</p>
