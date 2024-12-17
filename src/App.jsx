00a7d3af6530152c74cc1e1bd66583d84c2e813e
phenomme <medwards5154@gmail.com>
2024-12-16 20:12:17 -0500

    fetch web api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Macro from "./components/macro.jsx"
 import GoalsComponent from "./components/GoalBuilder.jsx"
 import './App.css'
+import FetchApi from './components/FetchAPI.jsx'
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
     <h1>Welcome to the Macro App!</h1>
     <Macro />
     <GoalsComponent />
+    <FetchApi />
     </>
   );
 }
